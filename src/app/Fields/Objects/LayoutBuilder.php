8a2aba36c311be4c980b8211ce5c7fccdf929b18
Dmitryfor <dimaportnov2000@gmail.com>
2020-07-18 18:02:04 +0300

    layout builder separately and make styles, scripts

---
+++ src/app/Fields/Objects/LayoutBuilder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Fields\Objects;
+
+use StoutLogic\AcfBuilder\FieldsBuilder;
+use App\Fields\Templates\Columns;
+use App\Fields\Templates\CallToAction;
+use App\Fields\Templates\BlockGrid;
+use App\Fields\Templates\SplitContent;
+
+class LayoutBuilder {
+
+	public function __construct() {
+
+		/**
+		 * Layout Builder
+		 */
+        
+		$layoutBuilder = new FieldsBuilder('layout_builder', [
+			'style' => 'seamless'
+        ]);
+        
+        $layoutBuilder
+        
+			->addFlexibleContent('templates', [
+				'label'					=>	'Layout Builder',
+				'button_label'	=>	'Add Template'
+            ])
+            
+                ->addLayout(Columns::getFields())
+                
+                ->addLayout(CallToAction::getFields())
+                
+			->setLocation('post_type', '==', 'page')
+				->or('post_type', '==', 'post')
+				->or('post_type', '==', 'ssm_article');
+
+		// Register Layout Builder
+		add_action('acf/init', function() use ($layoutBuilder) {
+			acf_add_local_field_group($layoutBuilder->build());
+		});
+
+	}
+
+}
--- src/app/Fields/Objects/Shared.php
+++ src/app/Fields/Objects/Shared.php
@@ -3,12 +3,6 @@
 namespace App\Fields\Objects;
 
 use StoutLogic\AcfBuilder\FieldsBuilder;
-use App\Fields\Templates\Columns;
-use App\Fields\Templates\CallToAction;
-use App\Fields\Templates\BlockGrid;
-use App\Fields\Templates\SplitContent;
-use App\Fields\Templates\Gallery;
-use App\Fields\Templates\RelatedContent;
 use App\Fields\Lists\Buttons;
 use App\Fields\Components\Header;
 use App\Fields\Options\Background;
@@ -27,6 +21,7 @@ class Shared {
 		$heroUnit = new FieldsBuilder('hero_unit', [
 			'position' => 'acf_after_title'
 		]);
+		
 		$heroUnit
 			->addTab('Content')
 				->addFields(Header::getFields())
@@ -42,34 +37,6 @@ class Shared {
 		});
 
 		/**
-		 * Layout Builder
-		 * @author Rich Staats <rich@secretstache.com>
-		 * @since 3.0.0
-		 * @todo Link to Team Snippet Code
-		 */
-		$layoutBuilder = new FieldsBuilder('layout_builder', [
-			'style' => 'seamless'
-		]);
-		$layoutBuilder
-			->addFlexibleContent('templates', [
-				'label'					=>	'Layout Builder',
-				'button_label'	=>	'Add Template'
-			])
-				->addLayout(Columns::getFields())
-				->addLayout(CallToAction::getFields())
-				// ->addLayout(RelatedContent::getFields())
-				// ->addLayout(BlockGrid::getFields())
-				// ->addLayout(SplitContent::getFields())
-				// ->addLayout(Gallery::getFields())
-			->setLocation('post_type', '==', 'page')
-				->or('post_type', '==', 'ssm_article');
-
-		// Register Layout Builder
-		add_action('acf/init', function() use ($layoutBuilder) {
-			acf_add_local_field_group($layoutBuilder->build());
-		});
-
-		/**
 		 * Inline Styles
 		 * @author Rich Staats <rich@secretstache.com>
 		 * @since 3.0.0
@@ -79,19 +46,19 @@ class Shared {
 			'menu_order'	=>	5
 		]);
 		$inlineStyles
-			->addField('inline_styles', 'acf_code_field', [
+			->addField('page_inline_styles', 'acf_code_field', [
 				'label'	=>	'CSS Editor',
 				'mode' 	=> 'css',
 				'theme'	=> 'monokai'
 			])
 			->setLocation('post_type', '==', 'page')
+				->or('post_type', '==', 'post')
 				->or('post_type', '==', 'ssm_article');
 
 		// Register Inline Styles
 		add_action('acf/init', function() use ($inlineStyles) {
 			acf_add_local_field_group($inlineStyles->build());
 		});
-
 
 		/**
 		 * Inline Scripts
@@ -103,19 +70,19 @@ class Shared {
 			'menu_order'	=>	6
 		]);
 		$inlineScripts
-			->addField('inline_scripts', 'acf_code_field', [
+			->addField('page_inline_scripts', 'acf_code_field', [
 				'label'	=>	'JS Editor',
-				'mode' 	=> 'js',
+				'mode' 	=> 'javascript',
 				'theme'	=> 'monokai',
 			])
 			->setLocation('post_type', '==', 'page')
+				->or('post_type', '==', 'post')
 				->or('post_type', '==', 'ssm_article');
 
 		// Register Inline Scripts
 		add_action('acf/init', function() use ($inlineScripts) {
 			acf_add_local_field_group($inlineScripts->build());
 		});
-
 
 		/**
 		 * Facebook Conversion Pixel
@@ -140,6 +107,7 @@ class Shared {
 				->addChoice('fbq("track", "AddPaymentInfo");', 'Added Payment Info')
 				->addChoice('fbq("track", "Purchase");', 'Made a Purchase')
 			->setLocation('post_type', '==', 'page')
+				->or('post_type', '==', 'post')
 				->or('post_type', '==', 'ssm_article');
 
 		// Register Facebook Conversion Pixel
