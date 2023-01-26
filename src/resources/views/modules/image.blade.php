0aecce6de41a366e80d33d9b2ef7c7729f410d20
Alex Smulsky <alex@secretstache.com>
2023-01-26 14:53:04 +0200

    module image external url fix

--- src/resources/views/modules/image.blade.php
+++ src/resources/views/modules/image.blade.php
@@ -6,7 +6,7 @@
 
             <a href="{!! get_permalink( $module['option_image_link_page_id'] ) !!}">
 
-        @elseif( $source == 'custom' && $module['option_image_link_url'] )
+        @elseif( $source == 'external' && $module['option_image_link_url'] )
 
             <a href="{!! $module['option_image_link_url'] !!}" target="_blank">
         
